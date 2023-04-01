c0bbc8f2a161ba448598109ec7d64f20c88acf8a
T1m4ay <tero4y@gmail.com>
2023-04-01 09:53:52 +0600

    modified:   src/App.js 	modified:   src/components/Icons/building.svg 	modified:   src/components/Main/Features/Features.js 	modified:   src/components/Main/Features/Features.module.css 	modified:   src/components/Main/Main.js

--- src/App.js
+++ src/App.js
@@ -11,17 +11,32 @@ function App() {
       {
         id: 1,
         title: "IITU",
-        icon: icod,
+        sum: 100000,
       },
       {
         id: 2,
         title: "Salary",
-        icon: 2,
+        sum: 150000,
       },
       {
         id: 3,
         title: "Other",
-        icon: 3,
+        sum: 30000,
+      },
+      {
+        id: 4,
+        title: "Other",
+        sum: 0,
+      },
+      {
+        id: 5,
+        title: "Other",
+        sum: 0,
+      },
+      {
+        id: 6,
+        title: "Other",
+        sum: 0,
       },
     ]
   );
@@ -30,10 +45,92 @@ function App() {
     localStorage.setItem("income", JSON.stringify(income));
   }, [income]);
 
+  const [accounts, setAccounts] = useState(
+    JSON.parse(localStorage.getItem("accounts")) || [
+      {
+        id: 1,
+        title: "Kaspi",
+        sum: 25000,
+      },
+      {
+        id: 2,
+        title: "Halyk",
+        sum: 100000,
+      },
+      {
+        id: 3,
+        title: "Cash",
+        sum: 180000,
+      },
+    ]
+  );
+
+  useEffect(() => {
+    localStorage.setItem("accounts", JSON.stringify(accounts));
+  }, [accounts]);
+
+  const [expenses, setExpenses] = useState(
+    JSON.parse(localStorage.getItem("expenses")) || [
+      {
+        id: 1,
+        title: "Products",
+        maxSum: 30000,
+        totalSum: 15000,
+      },
+      {
+        id: 2,
+        title: "Clothing",
+        maxSum: 70000,
+        totalSum: 45000,
+      },
+      {
+        id: 3,
+        title: "Home",
+        maxSum: 30000,
+        totalSum: 30000,
+      },
+      {
+        id: 4,
+        title: "Phone",
+        maxSum: 10000,
+        totalSum: 0,
+      },
+      {
+        id: 5,
+        title: "Transport",
+        maxSum: 25000,
+        totalSum: 1500,
+      },
+      {
+        id: 6,
+        title: "Entertain",
+        maxSum: 40000,
+        totalSum: 47000,
+      },
+      {
+        id: 7,
+        title: "Services",
+        maxSum: 15000,
+        totalSum: 12000,
+      },
+    ]
+  );
+
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
+
   return (
     <div>
       <Header />
-      <Main income={income} setIncome={setIncome} />
+      <Main
+        income={income}
+        setIncome={setIncome}
+        accounts={accounts}
+        setAccounts={setAccounts}
+        expenses={expenses}
+        setExpense={setExpenses}
+      />
     </div>
   );
 }
