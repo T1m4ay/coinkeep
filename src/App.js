8f4bc1fd1723449a67a27f95e34208330b0c6760
T1m4ay <tero4y@gmail.com>
2023-04-05 02:18:55 +0600

    modified:   src/App.js 	modified:   src/components/Icons/building.svg 	new file:   src/components/Icons/cart4.svg 	new file:   src/components/Icons/cash-coin.svg 	modified:   src/components/Icons/component.js 	new file:   src/components/Icons/credit-card.svg 	new file:   src/components/Icons/emoji-smile.svg 	modified:   src/components/Icons/globe.svg 	new file:   src/components/Icons/house.svg 	new file:   src/components/Icons/phone.svg 	modified:   src/components/Icons/plus-circle.svg 	new file:   src/components/Icons/shop.svg 	new file:   src/components/Icons/taxi-front.svg 	modified:   src/components/Icons/wallet2.svg 	new file:   src/components/Icons/wrench.svg 	modified:   src/components/Main/Features/Features.js 	modified:   src/components/Main/Features/Features.module.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import { useEffect, useState } from "react";
 import storage from "./components/Icons/component";
-import icod from "./components/Icons/building.svg";
 
 function App() {
   const [income, setIncome] = useState(
@@ -12,31 +11,43 @@ function App() {
         id: 1,
         title: "IITU",
         sum: 100000,
+        icon: storage.iconsForIncome[1],
+        color: "rgb(0, 190, 238)",
       },
       {
         id: 2,
         title: "Salary",
         sum: 150000,
+        icon: storage.iconsForIncome[4],
+        color: "rgb(0, 190, 238)",
       },
       {
         id: 3,
         title: "Other",
         sum: 30000,
+        icon: storage.iconsForIncome[2],
+        color: "rgb(0, 190, 238)",
       },
       {
         id: 4,
         title: "Other",
         sum: 0,
+        icon: storage.iconsForIncome[2],
+        color: "rgb(0, 190, 238)",
       },
       {
         id: 5,
         title: "Other",
         sum: 0,
+        icon: storage.iconsForIncome[2],
+        color: "rgb(0, 190, 238)",
       },
       {
         id: 6,
         title: "Other",
         sum: 0,
+        icon: storage.iconsForIncome[2],
+        color: "rgb(0, 190, 238)",
       },
     ]
   );
@@ -51,16 +62,22 @@ function App() {
         id: 1,
         title: "Kaspi",
         sum: 25000,
+        icon: storage.iconsForAccount[1],
+        color: "rgb(255, 205, 0)",
       },
       {
         id: 2,
         title: "Halyk",
         sum: 100000,
+        icon: storage.iconsForAccount[3],
+        color: "rgb(255, 205, 0)",
       },
       {
         id: 3,
         title: "Cash",
         sum: 180000,
+        icon: storage.iconsForAccount[2],
+        color: "rgb(255, 205, 0)",
       },
     ]
   );
@@ -74,47 +91,90 @@ function App() {
       {
         id: 1,
         title: "Products",
+        icon: storage.iconsForExpenses[1],
         maxSum: 30000,
         totalSum: 15000,
+        color: "rgb(194, 198, 202)",
+        fill: "",
       },
       {
         id: 2,
         title: "Clothing",
+        icon: storage.iconsForExpenses[2],
         maxSum: 70000,
         totalSum: 45000,
+        color: "rgb(194, 198, 202)",
+        fill: "",
       },
       {
         id: 3,
         title: "Home",
+        icon: storage.iconsForExpenses[3],
         maxSum: 30000,
         totalSum: 30000,
+        color: "rgb(194, 198, 202)",
+        fill: "",
       },
       {
         id: 4,
         title: "Phone",
+        icon: storage.iconsForExpenses[4],
         maxSum: 10000,
         totalSum: 0,
+        color: "rgb(194, 198, 202)",
+        fill: "",
       },
       {
         id: 5,
         title: "Transport",
+        icon: storage.iconsForExpenses[5],
         maxSum: 25000,
         totalSum: 1500,
+        color: "rgb(194, 198, 202)",
+        fill: "",
       },
       {
         id: 6,
         title: "Entertain",
+        icon: storage.iconsForExpenses[6],
         maxSum: 40000,
         totalSum: 47000,
+        color: "rgb(194, 198, 202)",
+        fill: "",
       },
       {
         id: 7,
         title: "Services",
+        icon: storage.iconsForExpenses[7],
         maxSum: 15000,
         totalSum: 12000,
+        color: "rgb(194, 198, 202)",
+        fill: "",
       },
     ]
   );
+
+  expenses.map((expenses) => {
+    if (
+      expenses.maxSum == expenses.totalSum ||
+      expenses.maxSum > expenses.totalSum
+    ) {
+      expenses.color = "green";
+      expenses.fill =
+        "linear-gradient(to top, " +
+        expenses.color +
+        " " +
+        Math.round((expenses.totalSum / expenses.maxSum) * 100) +
+        "%, rgb(194, 198, 202) " +
+        Math.round((expenses.totalSum / expenses.maxSum) * 100) +
+        "%)";
+      console.log(expenses.fill);
+    } else {
+      expenses.color = "red";
+      expenses.fill = "linear-gradient(to top, red 100%, transparent 0%";
+      console.log(expenses.fill);
+    }
+  });
 
   useEffect(() => {
     localStorage.setItem("expenses", JSON.stringify(expenses));
